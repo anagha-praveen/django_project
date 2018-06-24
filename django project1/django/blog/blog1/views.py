c2cda61c1a44c5fba0205dd1921b4ac2a002492c
PADMALEELA <sreekuttyptr@gmail.com>
2018-06-24 19:39:14 +0530

    django

---
+++ django project1/django/blog/blog1/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from . import forms
+from . import models
+from django.http import HttpResponse
+import feedparser
+import re
+
+
+
+def index(request):
+    return render( request, 'git1/index.html')
+
+#View for the Redirecting page -- This is where I want to catch the text box input
+def results(request):
+    if 'ln' in request.GET:
+    	try:
+    			feed = feedparser.parse(request.GET['ln'])
+    			for entry in feed.entries:
+    				s=(entry.description)
+    				r=(entry.summary)
+    				r= re.sub('<.*?>','',entry.summary)
+    			for entry in feed.entries:
+    				r=r+entry.link
+    			models.link(url=request.GET['ln']).save()
+    			return HttpResponse(r)
+    	except KeyError:
+    			return HttpResponse("invalid rss feed")
+    else:
+    	k='empty'
+    	return HttpResponse(k)							
+
+
+
